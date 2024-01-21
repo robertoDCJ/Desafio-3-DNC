523956ff4adbec3ea18fdaff2403ea297cb00280
robertoDCJ <robertox70@hotmail.com>
2024-01-21 18:40:39 -0300

    feat(ToDoList): Adicionado a funcionalidade de editar o item

--- my-app/src/components/ToDoList/ToDoList.jsx
+++ my-app/src/components/ToDoList/ToDoList.jsx
@@ -10,7 +10,21 @@ const ToDoList = () => {
     const [items, setItems] = useState([])
     const [newInput, setNewInput] = useState('')
     const [itemToDelete, setItemToDelete] = useState(null)
+    const [itemToEdit, setItemToEdit] = useState(null)
+    const [editedText, setEditedText] = useState('');
     const updatedItems = [...items]
+
+    const toggleEdit = (index) => {
+        setItemToEdit(itemToEdit === index ? null : index)
+        setEditedText(itemToEdit !== index ? items[itemToEdit].text : '')
+    }
+
+    const editing = (index) => {
+        updatedItems[index].text = editedText
+        setItems(updatedItems)
+        setItemToEdit(null)
+        setEditedText('')
+    }
 
     const toggleDelet = (index) => {
         setItemToDelete(itemToDelete === index ? null : index)
@@ -52,7 +66,7 @@ const ToDoList = () => {
                                 <img id='check' onClick={() => toggleCheck(index)} src={item.checked ? checked : check} alt='check' />
                             </div>
                             <div>
-                                <img src={pen} alt='editar' />
+                                <img onClick={() => toggleEdit(index)} src={pen} alt='editar' />
                                 <img onClick={() => toggleDelet(index)} src={dump} alt='deletar' />
                             </div>
                         </div>
@@ -62,6 +76,7 @@ const ToDoList = () => {
                     <input type="text" id='newInput' placeholder='Nova tarefa...' value={newInput} onChange={(e) => setNewInput(e.target.value)} />
                     <img onClick={Add} src={plus} alt="adicionar" />
                 </div>
+
                 {itemToDelete !== null && (
                     <div key={itemToDelete} className='to-do-list__delet'>
                         <h1>Deseja excluir esse item?</h1>
@@ -69,6 +84,22 @@ const ToDoList = () => {
                         <div>
                             <button onClick={() => toggleDelet(null)}>Não</button>
                             <button onClick={() => deleting(itemToDelete)}>Sim</button>
+                        </div>
+                    </div>
+                )}
+                
+                {itemToEdit !== null && (
+                    <div key={itemToEdit} className='to-do-list__delet'>
+                        <h1>Deseja editar esse item?</h1>
+                        <input
+                            type="text"
+                            value={editedText}
+                            placeholder={editedText}
+                            onChange={(e) => setEditedText(e.target.value)}
+                        />
+                        <div>
+                            <button onClick={() => toggleEdit(null)}>Não</button>
+                            <button onClick={() => editing(itemToEdit)}>Sim</button>
                         </div>
                     </div>
                 )}
